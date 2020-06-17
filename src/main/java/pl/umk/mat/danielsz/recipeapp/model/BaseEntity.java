8d7cbaf24247dfbbb3afa3374e8de53abac61092
Daniel Szatkowski <danielszatkowski432@gmail.com>
2020-06-17 19:23:30 +0200

    BaseEntity created

---
+++ src/main/java/pl/umk/mat/danielsz/recipeapp/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package pl.umk.mat.danielsz.recipeapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+}
