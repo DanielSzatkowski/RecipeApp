f5e5f614f719a95e1e46dc50748d48facaad8910
Daniel Szatkowski <danielszatkowski432@gmail.com>
2020-06-17 19:05:09 +0200

    project structure initialized

---
+++ src/test/java/pl/umk/mat/danielsz/recipeapp/RecipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umk.mat.danielsz.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
